851d02e75faaa48958ee0f86f706283ca6194b88
Apin Ridwan <apinrdw@gmail.com>
2017-07-06 17:16:02 +0700

    fix bug

--- recipes/create_db_template.rb
+++ recipes/create_db_template.rb
@@ -1,6 +1,6 @@
 execute :create_postgis_template do
 	user "postgres"
-	command "createdb -E UTF8 --locale=en_US.utf8 -T template0 #{node[:postgis][:template][:name]"
+	command "createdb -E UTF8 --locale=en_US.utf8 -T template0 #{node[:postgis][:template][:name]}"
 	not_if "psql -qAt --list | grep '^#{node[:postgis][:template][:name]}\|'", :user => 'postgres'
 	action :run
 end
